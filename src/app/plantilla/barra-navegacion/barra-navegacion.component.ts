b272f0d73d2b0cf5018939ec2c010b8f0db6de7f
Your Name <you@example.com>
2021-12-09 09:18:17 -0500

    make it better

---
+++ src/app/plantilla/barra-navegacion/barra-navegacion.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ModeloDatos } from 'src/app/modelos/datos.modelo';
+import { ModeloIdentificar } from 'src/app/modelos/identificar.modelo';
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
+
+@Component({
+  selector: 'app-barra-navegacion',
+  templateUrl: './barra-navegacion.component.html',
+  styleUrls: ['./barra-navegacion.component.css']
+})
+export class BarraNavegacionComponent implements OnInit {
+
+  datos?:ModeloDatos = new ModeloDatos();
+  sesionIniciada?: boolean=false;
+  subs: Subscription = new Subscription();
+  nombre?:string="diana";
+
+  constructor(private servicioSeguridad: SeguridadService) {
+    
+    this.subs= this.servicioSeguridad.ObtenerDatosUsuarioSesion().subscribe((datos:ModeloIdentificar)=>{
+      this.datos=datos.datos;
+      this.nombre=this.datos?.nombre;
+    })
+ 
+
+   }
+
+  ngOnInit(): void {
+    this.subs= this.servicioSeguridad.ObtenerDatosUsuarioSesion().subscribe((datos:ModeloIdentificar)=>{
+      this.sesionIniciada = datos.estaIdentificado;
+    })
+  }
+
+
+}
