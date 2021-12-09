1bd6bd9c2c62089eb4f45bcece9c35250e960e29
Your Name <you@example.com>
2021-12-09 11:42:54 -0500

    6

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/frontend-pedidos'));
+app.use(express.static('./frontend-pedidos'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/frontend-pedidos/'}),
+    res.sendFile('index.html', {root: '/frontend-pedidos/'}),
 );
 
 // Start the app by listening on the default Heroku port
